b3e1b007c008aaa9ea563e24a04a597500d9e581
Euler Chavez Hernandez <EulerChavez@outlook.com>
2017-04-25 23:33:34 -0500

    SignalR para "tiempo real" en graficas, no se pudo implementar con los recursos de la computadora

---
+++ PerfSurf/Counters/PerfCounterService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PerfSurf.Counters {
+
+    public class PerfCounterService {
+
+        public PerfCounterService() {
+
+            _counters = new List<PerfCounterWrapper>();
+
+            _counters.Add(new PerfCounterWrapper("Processor", "Processor", "% Processor Time", "_Total"));
+
+            _counters.Add(new PerfCounterWrapper("Paging", "Memory", "Pages/sec"));
+
+            _counters.Add(new PerfCounterWrapper("Disk", "PhysicalDisk", "% Disk Time", "_Total"));
+
+        }
+
+        public dynamic GetResults() {
+
+            return _counters.Select(r => new { name = r.Name, value = r.Value });
+
+        }
+
+        List<PerfCounterWrapper> _counters;
+
+    }
+
+}
--- PerfSurf/Hubs/PerfHub.cs
+++ PerfSurf/Hubs/PerfHub.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+using PerfSurf.Counters;
 
 namespace PerfSurf.Hubs {
 
     public class PerfHub : Hub {
+
+        public PerfHub() {
+
+            StartCounterCollection();
+
+        }
+
+        private void StartCounterCollection() {
+
+            var task = Task.Factory.StartNew(async () => {
+
+                //var perfService = new PerfCounterService();
+
+                while (true) {
+
+                    //var results = perfService.GetResults();
+
+                    Clients.All.newCounters(new[] {
+                          new { name = "Euler", value = new Random().Next(0, 100) }
+                        , new { name = "Chavez", value = new Random().Next(0, 100) }
+                        , new { name = "Hernandez", value = new Random().Next(0, 100) } }.AsEnumerable());
+
+                    await Task.Delay(2000);
+
+                }
+
+            }, TaskCreationOptions.LongRunning);
+
+        }
 
         public void Send(string message) {
 
